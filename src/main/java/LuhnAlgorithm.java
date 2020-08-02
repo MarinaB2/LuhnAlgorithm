4a2f0fd8d27f0670881252465a81b3d977bd7333
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-08-02 12:29:31 +0200

    Added some comments

--- src/main/java/LuhnAlgorithm.java
+++ src/main/java/LuhnAlgorithm.java
@@ -6,15 +6,22 @@ public class LuhnAlgorithm {
     public static  int expectedDigit;
     public static int providedDigit;
     public static int total = 0;
-    //76578998765501
+    //76578998765501  (A valid number but not 16 digits)
+    //2347669084563287 (A valid number with 16 digits)
+    //2347669084563288 (Not valid number)
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number: ");
-        String userInput = scanner.nextLine();
 
+        /*Using the trim method in case the user inter number with extra whitespaces in
+        the beginning or the end of the String*/
+        String userInput = scanner.nextLine().trim();
+
+        //Get the last character(number) of the String
         providedDigit = Integer.parseInt(userInput.substring(userInput.length() - 1));
+
         boolean isValidNumber =validatCreditNumber(userInput, providedDigit);
         boolean isValidLength = validateLength(userInput);
 
@@ -28,9 +35,9 @@ public class LuhnAlgorithm {
                 System.out.println("Checksum: Not Valid "  );
             }
             if(isValidLength){
-                System.out.println("Digits: " + userInput.length() + "( Credit card )");
+                System.out.println("Digits: " + userInput.length() + " " + "( Credit card )");
             }else {
-                System.out.println("Digits: " + userInput.length() + "( Not credit card! A credit card is 16 digits )");
+                System.out.println("Digits: " + userInput.length() + " " + "( Not credit card! A credit card has 16 digits )");
             }
         }
 
@@ -44,6 +51,8 @@ public class LuhnAlgorithm {
         for (int i = 0; i < input.length(); i++){
             inputToInt[i] = Integer.parseInt(input.substring(i, i+1));
         }
+
+        //Start the loop from the right and double each other digit
         for (int i = inputToInt.length - 2; i >= 0; i = i - 2){
             int currentValue = inputToInt[i];
             currentValue = currentValue * 2;
@@ -55,10 +64,12 @@ public class LuhnAlgorithm {
             inputToInt[i] = currentValue;
         }
 
+       //Add each element of the array together to get a so that we can get one big number
         for(int i = 0; i < inputToInt.length -1 ; i++){
             total += inputToInt[i];
         }
 
+      // To check the check digit I use a variable(expectedDigit) that equals the equation to get the luhn algorithm
       expectedDigit =(total * 9) % 10;
 
       if (expectedDigit == (total * 9) % 10){
@@ -69,6 +80,7 @@ public class LuhnAlgorithm {
             return false;
         }
     }
+
     public static boolean checkNumeric(String input){
 
             for (char character : input.toCharArray())
